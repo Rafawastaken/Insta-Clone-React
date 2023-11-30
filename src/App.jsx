c2ea7f552c77975d5e83cfeba864c21120fbd964
rafa <jrafapimenta@gmail.com>
2023-11-30 23:26:08 +0000

    auth route/homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
   return (
     <>
-      <Button>Teste</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
     </>
   );
 }
